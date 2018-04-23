5002deed6b15403a4302eda732b5a1d49c22c57e
Andres Ruiz <rb.andres98@gmail.com>
2018-04-22 19:18:21 -0500

    Set Points

---
+++ Assets/David's Drunk/Scripts/Character/PlayerPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPoints : MonoBehaviour {
+    public Text pointsText;
+    GameObject player;
+    //Script para calcular los puntos del jugador
+    private void Start()
+    {
+        player = GameObject.Find("David");
+    }
+
+    // Los puntos estan basados en la posición en el eje z del jugador * 10
+    void Update ()
+    {
+        pointsText.text = "Puntos:\n " + (player.transform.position.z * 10).ToString("0");
+	}
+}
